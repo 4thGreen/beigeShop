9b74ae4d17df889a78a390109f5ed2fcf620106f
rissins <tnalsqqq@naver.com>
2021-10-03 21:21:13 +0900

    feat(shop): 주문내역, 회원정보수정, 장바구니 DTO  구현

---
+++ src/main/java/order/OrderDTO.java
@@ -0,0 +1,78 @@
+package order;
+
+public class OrderDTO {
+    private Long orderNumber;   // 주문번호
+    private String productImage;// 상품이미지
+    private String productInfo; // 상품정보
+    private int amount;         // 수량
+    private int price;          // 가격
+    private String orderStatus; // 주문처리상태
+    private String orderCancel; // 취소/교환/환불 상태
+
+
+    public OrderDTO(Long orderNumber, String orderStatus, String orderCancel, int amount, int price, String productInfo, String productImage) {
+        this.orderNumber = orderNumber;
+        this.orderStatus = orderStatus;
+        this.orderCancel = orderCancel;
+        this.amount = amount;
+        this.price = price;
+        this.productInfo = productInfo;
+        this.productImage = productImage;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public Long getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Long orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderCancel() {
+        return orderCancel;
+    }
+
+    public void setOrderCancel(String orderCancel) {
+        this.orderCancel = orderCancel;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProductInfo() {
+        return productInfo;
+    }
+
+    public void setProductInfo(String productInfo) {
+        this.productInfo = productInfo;
+    }
+}
---
+++ src/main/java/tset/test1.java
@@ -0,0 +1,7 @@
+package tset;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
