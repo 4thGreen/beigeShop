86017a08694f3c846a2c34f9b33bb1b678e79a07
rissins <tnalsqqq@naver.com>
2021-10-17 00:51:41 +0900

    feat(shop): 회원가입 추가

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -1,9 +1,6 @@
 package user;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,7 +32,7 @@ public class UserDAO {
             if (rs.next()) {
                 if (rs.getString(1).equals(userPassword)) {
                     return 1; // 로그인 성공
-                }else {
+                } else {
                     return 0; // 비밀번호 불일치
                 }
             }
@@ -46,13 +43,13 @@ public class UserDAO {
         return -2;
     }
 
-    public List<String> view(String userID) {
+    public UserDTO view(String userID) {
         String SQL = "SELECT * FROM SHOPUSER WHERE ID = ?";
         try {
             pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1, userID);
             rs = pstmt.executeQuery();
-            while (rs.next()) {
+            if (rs.next()) {
                 String id = rs.getString("ID");
                 String password = rs.getString("PASSWORD");
                 String name = rs.getString("NAME");
@@ -61,13 +58,9 @@ public class UserDAO {
                 String mobileNumber = rs.getString("MOBILENUMBER");
                 String phoneNumber = rs.getString("PHONENUMBER");
                 String email = rs.getString("EMAIL");
-                list.add(id);
-                list.add(password);
-                list.add(name);
-                list.add(address);
-                list.add(mobileNumber);
-                list.add(phoneNumber);
-                list.add(email);
+                UserDTO userDTO = new UserDTO(id, password, name, address, mobileNumber, phoneNumber, email);
+
+                return userDTO;
             }
 
 //            return -1; //아이디가 없음
@@ -76,11 +69,76 @@ public class UserDAO {
         } finally {
             dbClose();
         }
-        return list;
+        return null;
     }
 
 
-    public int join(UserDTO user){
+    public UserDTO viewAddress(String userID) {
+        String SQL = "SELECT REGEXP_SUBSTR(ADDRESS, '[^/]+',1,1) AS POSTCODE , REGEXP_SUBSTR(ADDRESS, '[^/]+',1,2) AS ADDRESS, REGEXP_SUBSTR(ADDRESS, '[^/]+',1,3) AS EXTRAADDRESS, REGEXP_SUBSTR(ADDRESS, '[^/]+',1,4) AS DETAILADDRESS FROM SHOPUSER WHERE ID = ?";
+//        ResultSetMetaData rsmd;
+        try {
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String postcode = rs.getString("POSTCODE");
+                String address = rs.getString("ADDRESS");
+                String detailAddress = rs.getString("DETAILADDRESS");
+                String extraAddress = rs.getString("EXTRAADDRESS");
+//                rsmd = rs.getMetaData();
+//                System.out.println("rsmd.getColumnName(1) = " + rsmd.getColumnName(1));
+//                System.out.println("rsmd.getColumnName(1) = " + rsmd.getColumnName(2));
+//                System.out.println("rsmd.getColumnName(1) = " + rsmd.getColumnName(3));
+//                System.out.println("rsmd.getColumnName(1) = " + rsmd.getColumnName(4));
+
+
+                UserDTO userDTO = new UserDTO(postcode, address, detailAddress, extraAddress);
+
+                return userDTO;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dbClose();
+        }
+        return null;
+    }
+
+    public UserDTO viewNumber(String userID) {
+        String SQL = "SELECT REGEXP_SUBSTR(PHONENUMBER, '[^-]+',1,1)AS PHONENUMBER1 , REGEXP_SUBSTR(PHONENUMBER, '[^-]+',1,2)AS PHONENUMBER2, REGEXP_SUBSTR(PHONENUMBER, '[^-]+',1,3)AS PHONENUMBER3, REGEXP_SUBSTR(MOBILENUMBER, '[^-]+',1,1)AS MOBILENUMBER1, REGEXP_SUBSTR(MOBILENUMBER, '[^-]+',1,2)AS MOBILENUMBER2, REGEXP_SUBSTR(MOBILENUMBER, '[^-]+',1,3)AS MOBILENUMBER3 FROM SHOPUSER WHERE ID = ?";
+        ResultSetMetaData rsmd1;
+        try {
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String phoneNumber1 = rs.getString("PHONENUMBER1");
+                String phoneNumber2 = rs.getString("PHONENUMBER2");
+                String phoneNumber3 = rs.getString("PHONENUMBER3");
+                String mobileNumber1 = rs.getString("MOBILENUMBER1");
+                String mobileNumber2 = rs.getString("MOBILENUMBER2");
+                String mobileNumber3 = rs.getString("MOBILENUMBER3");
+
+
+                UserDTO userDTO = new UserDTO(phoneNumber1, phoneNumber2, phoneNumber3, mobileNumber1, mobileNumber2, mobileNumber3);
+
+                return userDTO;
+            }
+
+//            return -1; //아이디가 없음
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dbClose();
+        }
+        return null;
+    }
+
+
+    public int register(UserDTO user) {
         String SQL = "INSERT INTO SHOPUSER VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             pstmt = conn.prepareStatement(SQL);
@@ -92,7 +150,7 @@ public class UserDAO {
             pstmt.setString(6, user.getMobileNumber());
             pstmt.setString(7, user.getUserEmail());
             return pstmt.executeUpdate();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             dbClose();
@@ -104,8 +162,7 @@ public class UserDAO {
     //회원 정보 수정
     public int updateMember(UserDTO user) {
 //    public int updateMember(String id, String password, String name, String address, long mobileNumber, long phoneNumber, String email) {
-        String sql = "UPDATE SHOPUSER SET PASSWORD = ?, NAME = ?, ADDRESS = ?, MOBILENUMBER = ?, PHONENUMBER = ?, EMAIL = ? WHERE ID = ?" ;
-
+        String sql = "UPDATE SHOPUSER SET PASSWORD = ?, NAME = ?, ADDRESS = ?, MOBILENUMBER = ?, PHONENUMBER = ?, EMAIL = ? WHERE ID = ?";
 
 
         try {
@@ -113,14 +170,14 @@ public class UserDAO {
             pstmt.setString(1, user.getUserPassword());
             pstmt.setString(2, user.getUserName());
             pstmt.setString(3, user.getUserAddress());
-            pstmt.setString(4, user.getPhoneNumber());
-            pstmt.setString(5, user.getMobileNumber());
+            pstmt.setString(4, user.getMobileNumber());
+            pstmt.setString(5, user.getPhoneNumber());
             pstmt.setString(6, user.getUserEmail());
             pstmt.setString(7, user.getUserId());
 
             pstmt.executeUpdate();
             System.out.println("회원정보수정성공");
-            return  1;
+            return 1;
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("updateMember() Exception!!!");
@@ -132,12 +189,14 @@ public class UserDAO {
 
     public void dbClose() {
         try {
-            if(rs != null) rs.close();
-            if(pstmt != null) pstmt.close();
-            if(conn != null) conn.close();
+            if (rs != null) rs.close();
+            if (pstmt != null) pstmt.close();
+            if (conn != null) conn.close();
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("dbClose() Exception!!!");
         }
     }
+
+
 }
